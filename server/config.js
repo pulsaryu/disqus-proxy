00a5d68c7cc947056132f5c4b26c4770f2d00ca3
szh-laptop-hp <24031shp@sina.com>
2019-04-21 22:45:25 +0800

    Updated

--- server/config.js
+++ server/config.js
@@ -1,15 +1,17 @@
 module.exports = {
-  // 服务端端口，需要与disqus-proxy前端设置一致
+  // 服务端端口，需要与 disqus-proxy 前端设置一致, 如果你不确定就不要修改啦
   port: 5050,
 
+  // disqus public key (到这里查看: https://disqus.com/api/applications/)
   api_key: '',
-  // 你的diqus secret key
+
+  // diqus secret key (到这里查看: https://disqus.com/api/applications/)
   api_secret: '',
 
   // 你的disqus名称
   username: 'szhshp',
 
-  // 服务端socks5代理转发，便于在本地测试，生产环境通常为null
+  // 服务端 socks5 代理转发，便于在本地测试，生产环境通常为 null
   // socks5Proxy: {
   //   host: 'localhost',
   //   port: 1086
