618d2b1340fa9c3dd5b949f8c48735ed7dfa6253
szh-laptop-hp <24031shp@sina.com>
2019-11-11 23:30:37 +0800

    Init

--- server/README.md
+++ server/README.md
@@ -1,80 +1,83 @@
-# Disqus-Proxy - Server
+# Disqus-Proxy: Server
+
+## TODO
+
+- [ ] 后端
+  - [x] Logger Refactory
+  - [ ] 写一个 approve/spam 功能
+  - [ ] 写一个 vote 功能
+  - [ ] 整理一下 config 的详细
+  - [ ] 整理一下 readme 里面的 config 的详细
+  - [ ] Readme 最好全部整理一下
+- [ ] 前端
+  - [ ] 写一个管理页面
+  - [ ] 添加一下 Vote 功能
 
 ## API
 
-- 获取所有文章
-  - GET `http://localhost:5050/api/getThreads`
-- 获取所有评论
-  - GET `http://localhost:5050/api/listPosts`
-- 获取特定文章评论
-  - GET `http://localhost:5050/api/getComments`
-  - 参数: 
-    - identifier: 文章 Indent, 这里一般就是网址
-- 添加评论 
-  - POST ``
-  - 参数:
-    - author_email: 邮箱名
-    - author_name: 用户名
-    - message: 正文内容
-    - parent: null 或者 parent 评论的 ID
-    - thread: 文章 ID, 注意这里不是 Ident, 只接受纯数字
+* 获取所有文章
+  + GET `/api/getThreads` 
+  + 参数: 
+    - include: `string[]` 
+      - 默认 `approved` , 可选选项: unapproved, approved, spam, deleted, flagged, highlighted
+* 获取所有评论
+  + GET `/api/listPosts` 
+* 获取特定文章评论
+  + GET `/api/getComments` 
+  + 参数: 
+    - identifier: `string` 
+      - 文章 Indent, 一般就是对应的 Post 的 url
+* 添加评论 
+  + POST `/api/createComment` 
+  + 参数:
+    - author_email: `string` 
+      - 邮箱名
+    - author_name: `string` 
+      - 用户名
+    - message: `string` 
+      - 正文内容
+    - parent: `null | number` 
+      - null 或者 parent 评论的 ID
+    - thread: `number` 
+      - 文章 ID, 注意这里不是 Ident, 只接受纯数字
 
-## Server Deployment
+
+
+## Dev
 
 1. 安装依赖
-    
-```
-npm install
-```
- 
-3. 配置 server 目录下的config.js
-    
-```js
-module.exports = {
-    // 服务端端口，需要与 disqus-proxy 前端设置一致
-    port: 5509,
-    // 你的 diqus secret key
-    api_secret: 'your secret key',
-    // 你的 disqus 名称，就是forum名称
-    username:'ciqu',
-    // 服务端 socks5 代理转发，便于在本地测试，生产环境通常为 null
-    socks5Proxy: null,
-    // 日志输出位置, 输出到文件或控制台 'file' | 'console'
-    log: 'console'
-}
-```
+    ``` 
+      npm install
+    ```
+2. 配置 server 目录下的config.js
+    ``` js
+    module.exports = {
+      // 服务端端口，需要与 disqus-proxy 前端设置一致
+      port: 5509,
+      // 你的 diqus secret key
+      api_secret: 'your secret key',
+      // 你的 disqus 名称，就是forum名称
+      username: 'ciqu',
+      // 服务端 socks5 代理转发，便于在本地测试，生产环境通常为 null
+      socks5Proxy: null,
+      // 日志输出位置, 输出到文件或控制台 'file' | 'console'
+      log: 'console'
+    }
+    ```
+    api secret key 需要在 [Disqus 的 API 页面](https://disqus.com/api/applications/)申请
 
-api secret key 需要在 [Disqus 的 API 页面](https://disqus.com/api/applications/)申请
+    另外需要到 `Settings => Community` 里开启访客评论
 
-另外需要到`Settings => Community` 里开启访客评论
+3. 启动服务
 
-4. 启动服务
+    ``` 
+    cd server
+    npm start
+    ```
 
-```
-cd server
-npm start
-```
-
-> 正常运行服务，关闭 ssh 的时候就会关闭服务器，因此可以考虑使用 forever 插件
+    > 正常运行服务，关闭 ssh 的时候就会关闭服务器，因此可以考虑使用 forever 插件
 
 5. 让服务跟随服务器自动启动  
-
->#### Forever 的使用
->
->安装见官方Repo: [    https://github.com/foreverjs/forever   ](https://github.com/foreverjs/forever)
->
->
->很简单，只需要学会启动和关闭两个命令即可。
->
->```
->forever start app.js
->forever stopall
->```
->
->#### 服务器自动启动
->
->参考另一篇文章:  [优雅地乱玩Linux-7-Cron实现程序自启动](http://szhshp.org/tech/2017/08/26/croninlinux.html)
-
 
 ## Debug
 
@@ -82,7 +85,7 @@ npm start
 
 进入 `\server` 执行:
 
-```
+``` 
 npm install
 nodemon index.js
 ```
@@ -90,3 +93,4 @@ nodemon index.js
 即可开启调试.
 
 确认开启服务端之后到浏览器输入 `http://localhost:5050/api/listPosts` 应该会显示所有评论数据
+
---
+++ server/src/index.js
@@ -0,0 +1,149 @@
+const Koa = require('koa');
+const rq = require('request-promise-native');
+const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
+const log4js = require('log4js');
+const cors = require('kcors');
+const config = require('./config');
+
+const app = new Koa();
+
+if (config.log === 'file') {
+  log4js.configure({
+    appenders: { 'disqus-proxy': { type: 'file', filename: 'disqusProxy.log' } },
+    categories: { default: { appenders: ['disqus-proxy'], level: 'info' } },
+  });
+}
+
+const logger = log4js.getLogger('disqus-proxy');
+app.use(bodyParser());
+app.use(cors());
+
+const req = {};
+
+router.get('/api/getThreads', async (ctx) => {
+  logger.info('Get Thread');
+  let result;
+
+  try {
+    const url = ['https://disqus.com/api/3.0/threads/list.json?',
+      'api_secret=',
+      config.api_secret,
+      '&forum=',
+      config.username,
+      '&thread:ident=',
+      encodeURIComponent(ctx.request.query.identifier),
+    ].join('');
+
+    logger.info(url);
+
+    result = await rq(Object.assign(req, {
+      method: 'GET',
+      url,
+      json: true,
+    }));
+  } catch (e) {
+    ctx.body = e.error;
+    logger.error(`Error when get thread:${JSON.stringify(e.error)}`);
+    return;
+  }
+  logger.info(`Get thread successfully with response code: ${result.code}`);
+  ctx.body = result;
+});
+
+router.get('/api/listPosts', async (ctx) => {
+  logger.info('Get Posts');
+  let result;
+  try {
+    const url = ['https://disqus.com/api/3.0/forums/listPosts.json?',
+      'api_key=', config.api_key,
+      (ctx.request.query.include === undefined) ? '' : ctx.request.query.include.split(',').map((e) => `@include=${e}`).join(''),
+      '&forum=', config.username,
+      '&limit=12'].join('');
+
+    logger.info(url);
+
+    result = await rq(Object.assign(req, {
+      method: 'GET',
+      url,
+      json: true,
+    }));
+  } catch (e) {
+    ctx.body = e.error;
+    logger.error(`Error when get comment:${JSON.stringify(e.error)}`);
+    return;
+  }
+  logger.info(`Get comments successfully with response code: ${result.code}`);
+  ctx.body = result;
+});
+
+router.get('/api/getComments', async (ctx) => {
+  logger.info(`Get Comments with identifier: ${ctx.request.query.identifier}`);
+  let result;
+
+  try {
+    const url = ['https://disqus.com/api/3.0/threads/listPosts.json?',
+      'api_secret=',
+      config.api_secret, '&forum=', config.username,
+      '&limit=100',
+      '&thread:ident=',
+      encodeURIComponent(ctx.request.query.identifier)].join('');
+
+    logger.info(url);
+
+    result = await rq(Object.assign(req, {
+      method: 'GET',
+      url,
+      json: true,
+    }));
+  } catch (e) {
+    ctx.body = e.error;
+    logger.error(`Error when get comment:${JSON.stringify(e.error)}`);
+    return;
+  }
+  logger.info(`Get comments successfully with response code: ${result.code}`);
+  ctx.body = result;
+});
+
+router.post('/api/createComment', async (ctx) => {
+  logger.info('Create Comment');
+  let result;
+  try {
+    logger.info(JSON.stringify(ctx.request.body));
+
+    result = await rq(Object.assign(req, {
+      url: 'https://disqus.com/api/3.0/posts/create.json',
+      method: 'POST',
+      form: Object.assign(ctx.request.body, {
+        api_key: 'E8Uh5l5fHZ6gD8U3KycjAIAk46f68Zw7C6eW8WSjZvCLXebZ7p0r1yrYDrLilk2F',
+      }),
+      json: true,
+    }));
+  } catch (e) {
+    logger.error(`Error when create comment:${JSON.stringify(e.error)}`);
+    ctx.body = e.error;
+    return;
+  }
+  ctx.body = result;
+  logger.info(`Create comment successfully with response code: ${result.code}`);
+});
+
+
+// TODO: Approve
+// TODO: Spam
+// TODO: Remove
+// TODO: Vote
+// TODO: Restore
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(config.port);
+
+console.log(`Disqus proxy server start at port: ${config.port}`);
+
+if (config.log === 'file') {
+  console.log('See disqus-proxy.log in current directory.');
+  logger.info(`Server start at port: ${config.port}`);
+}
