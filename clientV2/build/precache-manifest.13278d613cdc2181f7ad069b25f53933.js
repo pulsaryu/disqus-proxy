713c872f3d44eb0212ab4bf2e7417466e217bade
Dell.Office <24031shp@sina.com>
2019-11-18 14:27:29 +0800

    Add

---
+++ clientV2/build/precache-manifest.13278d613cdc2181f7ad069b25f53933.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1adce824e614c7b7c976185030b8d9fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32c74af9c986487b2c5f",
+    "url": "/static/css/2.10450d98.chunk.css"
+  },
+  {
+    "revision": "32c74af9c986487b2c5f",
+    "url": "/static/js/2.8a9794e9.chunk.js"
+  },
+  {
+    "revision": "cd98abac78e73ecedc94",
+    "url": "/static/js/main.a6b4aba5.chunk.js"
+  },
+  {
+    "revision": "b38336263a0a763576e9",
+    "url": "/static/js/runtime-main.4e64eb2d.js"
+  }
+]);
